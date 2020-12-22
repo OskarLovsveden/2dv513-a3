02cd0f9c6672aa7e91d047ce5c0765b250fcbc7e
ol222hf <ol222hf@student.lnu.se>
2020-12-22 13:56:32 +0100

    merge

--- client/src/components/CharacterListItem.tsx
+++ client/src/components/CharacterListItem.tsx
@@ -13,7 +13,6 @@ const CharacterListItem: React.FC<ICharacter> = ({ name, species, birth_planet, 
         <h2>{name}</h2>
       </IonLabel>
       <IonContent>
-<<<<<<< HEAD
         <IonModal isOpen={showModal} cssClass='my-custom-class'>
           <IonCard>
             <IonCardHeader>
@@ -40,42 +39,10 @@ const CharacterListItem: React.FC<ICharacter> = ({ name, species, birth_planet, 
               </IonList>
             </IonCardContent>
           </IonCard>
-          <IonButton size="small" color="medium" onClick={() => setShowModal(false)}>Close</IonButton>
+          <IonButton fill="outline" size="small" onClick={() => setShowModal(false)}>Close</IonButton>
         </IonModal>
-        <IonButton color="light" onClick={() => setShowModal(true)}>More info</IonButton>
+        <IonButton fill="outline" onClick={() => setShowModal(true)} class="ion-float-right">More info</IonButton>
       </IonContent>
-=======
-      <IonModal isOpen={showModal} cssClass='my-custom-class'>
-        <IonCard>
-          <IonCardHeader>
-            <IonCardTitle>
-              {name}
-            </IonCardTitle>
-          </IonCardHeader>
-          <IonCardContent>
-          <IonList>
-            <IonItem>
-              <h1>Species - {species}</h1>
-            </IonItem>
-            <IonItem>
-              <h1>Birth Planet - {birth_planet}</h1>
-            </IonItem>
-            <IonItem>
-              <h1>Apppear In</h1>
-            </IonItem>
-            <IonItem>
-            <IonList>
-              { appearsIn.map(a => (<IonItem key={a.episode}>Episode {a.episode} - {a.movie}</IonItem>)) }
-            </IonList>
-            </IonItem>
-          </IonList>
-          </IonCardContent>
-        </IonCard>
-        <IonButton fill="outline" size="small" onClick={() => setShowModal(false)}>Close</IonButton>
-      </IonModal>
-      <IonButton fill="outline" onClick={() => setShowModal(true)} class="ion-float-right">More info</IonButton>
-    </IonContent>
->>>>>>> bd07c7ca93374a76729c62fab5c62c5d5f4cc211
     </IonItem>
   );
 };
