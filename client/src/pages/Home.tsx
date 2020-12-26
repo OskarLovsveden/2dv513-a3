d6213ecdc4200a742e08b9d8c240fe509c0fa1a0
Viktor Ödman <vo222dq@student.lnu.se>
2020-12-26 16:04:11 +0100

    Fixing home

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -8,8 +8,12 @@ const Home: React.FC = () => {
         <IonToolbar>
           <IonTitle>Home</IonTitle>
         </IonToolbar>
-        <IonText color="primary" style={{ margin: "20px" }}>
-          <h1>Eyy sup you stupid neerds. This is our cool app with awesome stuff and FACTS! Fun FACTS!</h1>
+        <IonText color="secondary" style={{ margin: "20px" }}>
+          <h1>Hello and Welcome to the <IonText color="danger">Star Wars FC </IonText>APP</h1>
+        </IonText>
+        <IonText color="dark">
+          <h3>In this app you will find information about the Star Wars universe and its different characters</h3>
+          <h3>The information is gathered from different members of the Star Wars FC oraganisation</h3>
         </IonText>
       </IonHeader>
     </IonPage>
