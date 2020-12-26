92915bed8d5dea0f4fb8d974627ea18196ef6aff
ol222hf <ol222hf@student.lnu.se>
2020-12-26 16:14:24 +0100

    fix

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,4 +1,4 @@
-import { IonHeader, IonPage, IonText, IonTitle, IonToolbar } from '@ionic/react';
+import { IonContent, IonHeader, IonPage, IonText, IonTitle, IonToolbar } from '@ionic/react';
 import React from 'react';
 
 const Home: React.FC = () => {
@@ -6,16 +6,18 @@ const Home: React.FC = () => {
     <IonPage>
       <IonHeader>
         <IonToolbar>
-          <IonTitle>Home</IonTitle>
+          <IonTitle style={{ letterSpacing: "3px" }}>HOME</IonTitle>
         </IonToolbar>
-        <IonText color="secondary" style={{ margin: "20px" }}>
+      </IonHeader>
+      <IonContent fullscreen class="ion-padding">
+        <IonText color="secondary">
           <h1>Hello and Welcome to the <IonText color="danger">Star Wars FC </IonText>APP</h1>
         </IonText>
-        <IonText color="dark">
+        <IonText color="light">
           <h3>In this app you will find information about the Star Wars universe and its different characters</h3>
           <h3>The information is gathered from different members of the Star Wars FC oraganisation</h3>
         </IonText>
-      </IonHeader>
+      </IonContent>
     </IonPage>
   );
 };
